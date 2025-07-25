051b9f8604dee469f005a15b831d4de05ab6792c
Vinicius-Giovanni <viniciusgiovanni2003@gmail.com>
2025-07-25 17:19:59 -0300

    feat: add function that returns penultimate day

--- utils/get_info.py
+++ utils/get_info.py
@@ -35,3 +35,52 @@ def get_yesterday_date(format: str = '%d/%m/%Y', logger=None) -> str:
             'status': 'failure'
         })
         return None
+    
+@log_with_context(job='get_penultimate_date')
+def get_penultimate_date(parquet_folder: str, column: str = 'data_criterio', format: str = '%d/%m/%Y') -> str | None:
+    """
+    returns the penultimate date from the parquet files in the specified folder
+    """
+
+    folder_path = Path(parquet_folder)
+    all_files = list(folder_path.glob('*.parquet'))
+
+    if not all_files:
+        logger.warning(f'nenhum arquivo parquet encontrado em {parquet_folder}', extra={
+            'job': 'get_penultimate_date',
+            'status': 'no_files'
+        })
+
+        raise FileNotFoundError(f'nenhum arquivo parquet encontrado em {folder_path}')
+    
+    all_dates = []
+
+    for file in all_files:
+        # read the parquet file and extract the specified column
+        try:
+            df = pd.read_parquet(file, columns=[column])
+            all_dates.extend(df[column].dropna().tolist())
+            logger.info('procurando pela penúltima data', extra={
+                'job': 'get_penultimate_date',
+                'status': 'file_processed',
+                'file': str(file)
+            })
+        except Exception as e:
+            logger.critical(f'erro ao ler arquivo {file}: {e}', extra={
+                'job': 'get_penultimate_date',
+                'status': 'file_read_error',
+                'file': str(file)
+            })
+    
+    if len(all_dates) < 2:
+        logger.warning('menos de duas datas encontradas, não é possível determinar a penúltima data', extra={
+            'job': 'get_penultimate_date',
+            'status': 'not_enough_dates'
+        })
+        return None
+    
+    all_dates = pd.to_datetime(all_dates, errors='coerce', dayfirst=True)
+    unique_sorted_dates = sorted(set(all_dates), reverse=True)
+
+    penultimate = unique_sorted_dates[1] if len(unique_sorted_dates) > 1 else None # <<< acess the second date of the unique_sorted_dates
+    return penultimate.strftime(format) if penultimate else None      
