7ed41b570e6ddc1b3d56508dc4e8484bb6b5656f
Vinicius-Giovanni <viniciusgiovanni2003@gmail.com>
2025-07-28 13:46:04 -0300

    test(main): test for function 'teste'

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 from utils.get_info import get_yesterday_date
 from utils.config_logger import setup_logger
+from web_data_collector.olpn import teste
 
 logger = setup_logger(__name__)
 
 def main():
-    data = get_yesterday_date(format='%Q-%W-%Y', logger=logger)
-    logger.info(f'resultado retornado: {data}', extra={
-        'job': 'main',
-        'status': 'completed',
-    })
+    teste()
 
 
 if __name__ == "__main__":
