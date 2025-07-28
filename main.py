1bef943a495b489b24af6247ac61920fb94b5ced
Vinicius-Giovanni <viniciusgiovanni2003@gmail.com>
2025-07-28 16:41:41 -0300

    test(main.py): login_csv function test

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from utils.get_info import get_yesterday_date
 from utils.config_logger import setup_logger
-from web_data_collector.olpn import teste
+from web_data_collector.login import login_csi
+from config.settings import PASSWORD, EMAIL
 
 logger = setup_logger(__name__)
 
 def main():
-    teste()
+    logger.info('iniciando automacao web_data_collector', extra={
+        'job': 'main',
+        'status': 'started'
+    })
+    login_csi()
+    logger.info('finalizando automacao web_data_collector', extra={
+        'job': 'main',
+        'status': 'finished'
+    })
+
 
 
 if __name__ == "__main__":
