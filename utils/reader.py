178f9764ece14f9aa4f4031f59c3f8fd4e7348b5
Vinicius-Giovanni <viniciusgiovanni2003@gmail.com>
2025-07-31 10:52:56 -0300

    feat(reader.py): new functions - clear_dirs(refatoring): refatoring funcition clear_dirts supporting nested values - read_csv: reading files in the folder and storing/retuning them in a dataframe - export_as_parquet: reading the .csv and transformating for .parquet - forward_files_by_name: movs files from a source folder to specific destinations basead on their name as defined in the routing dictionary (case-insensitive)

--- utils/reader.py
+++ utils/reader.py
@@ -1,33 +1,36 @@
 # remote imports
 import shutil
-import os
+import pandas as pd
 from pathlib import Path
 import time
 from typing import Dict
+import os
+import shutil
 
 # local imports
 from utils.config_logger import setup_logger, log_with_context
-from config.settings import FILE_ROUTER
-
+from config.settings import CHUNKSIZE, PIPELINE_CONFIG
 
 # %(name)s <<< module name
 logger = setup_logger(__name__)
 
 @log_with_context(job='clear_dirs', logger=logger)
-def clear_dirs(dirs: Dict[str, Path]) -> None:
+def clear_dirs(dirs: Dict[str, any], prefix: str = '') -> None:
     """
     clear the directories specified in the dict. If not present, they are created.
     """
-    logger.info(f'iniciando limpeza dos diretorios: {list(dirs.keys())}', extra={
-        'job': 'clear_dirs',
-        'status': 'started'
-    })
-
     for name, folder in dirs.items():
+        full_name = f'{prefix}_{name}' if prefix else name
+
+        if isinstance(folder, dict):
+            clear_dirs(folder, prefix=full_name)
+            continue
+        
         try:
             if not folder.exists():
-                logger.warning(f'diretorio {name} ({folder}) nao existe. criando...', extra={
+                logger.warning(f'diretorio {full_name} ({folder}) nao existe. criando...', extra={
                     'job': 'clear_dirs',
+                    'status': 'creating folders'
                 })
                 folder.mkdir(parents=True, exist_ok=True)
                 continue
@@ -39,21 +42,21 @@ def clear_dirs(dirs: Dict[str, Path]) -> None:
                     elif item.is_dir():
                         shutil.rmtree(item)
                 except Exception as e:
-                    logger.exception(f'erro ao remover {item} em {name} ({folder}): {e}', extra={
+                    logger.exception(f'erro ao remover {item} em {full_name} ({folder})', extra={
                         'job': 'clear_dirs',
                         'status': 'failure'
                     })
-
-            logger.info(f'Diretório {name} ({folder}) limpo com sucesso.', extra={
+            logger.info(f'diretorio {full_name} ({folder}) limpo', extra={
                 'job': 'clear_dirs',
                 'status': 'success'
             })
-
+        
         except Exception as e:
-            logger.exception(f'erro ao processar diretorio {name} ({folder}): {e}', extra={
+            logger.exception(f'erro ao processar diretorio {full_name} ({folder})', extra={
                 'job': 'clear_dirs',
                 'status': 'failure'
             })
+
 
 @log_with_context(job='wait_download_csv', logger=logger)
 def wait_download_csv(dir: str | Path,
@@ -112,17 +115,121 @@ def wait_download_csv(dir: str | Path,
         'status': 'timeout'
     })
 
-@log_with_context(job='function_distribution', logger=logger)
-def function_distribution(path_folder: Path):
+@log_with_context(job='read_csv', logger=logger)
+def read_csv(path: Path, pipeline_key: str) -> pd.DataFrame:
+
+    logger.info(f'leitura arquivo: {path} pipeline: {pipeline_key}', extra={
+        'job': 'read_csv',
+        'status': 'started'
+    })
+
+    cfg = PIPELINE_CONFIG.get(pipeline_key)
+    if not cfg:
+        logger.critical(f'pipeline {pipeline_key} invalido', extra={
+            'job': 'read_csv',
+            'status': 'failure'
+        })
+        return pd.DataFrame()
+    
+    # Caso a conf do arquivo não existe, define como padrão o utf-8 e o separador ;
+    encoding = cfg.get('encoding', 'utf-8')
+    sep = cfg.get('sep', ';')
+
+    dataframes = []
+
+    if path.is_file() and path.suffix == '.csv':
+        files = [path]
+    elif path.is_dir():
+        files = list(path.glob('*.csv'))
+    else:
+        logger.critical(f'caminho {path} nao e um arquivo ou diretorio valido', extra={
+            'job': 'read_csv',
+            'status': 'failure'
+        })
+        return pd.DataFrame()
+    
+    for file in files:
+        try:
+            for chunk in pd.read_csv(
+                file,
+                encoding=encoding,
+                sep=sep,
+                chunksize=CHUNKSIZE,
+                low_memory=False,
+            ):
+                dataframes.append(chunk)
+        except Exception as e:
+            logger.error(f'erro ao ler arquivo {file}: {e}', extra={
+                'job': 'read_csv',
+                'status': 'failure'
+            })
+
+    return pd.concat(dataframes, ignore_index=True) if dataframes else pd.DataFrame()
+
+@log_with_context(job='export_as_parquet', logger=logger)
+def export_as_parquet(df: pd.DataFrame, output_folder: Path, pipeline_key: str, name: str):
+    cfg = PIPELINE_CONFIG.get(pipeline_key)
+    if not cfg:
+        logger.critical(f'pipeline "{pipeline_key}" nao encontrado nas configuracoes', extra={
+            'job': 'export_as_parquet',
+            'status': 'failure'
+        })
+        return
+    
+    output_file = output_folder/ f'{name}.parquet'
+
+    try:
+        df.to_parquet(output_file, index=False)
+        logger.info(f'sucesso ao exportar: {output_file.name} ({len(df):,} linhas)'.replace(',','.'))
+    except Exception as e:
+        logger.critical(f'erro ao exportar para parquet: {e}')
+
+@log_with_context(job='forward_files_by_name', logger=logger)
+def forward_files_by_name(input_folder: Path, file_router: dict):
     """
-    - searches for specific strings within the .csv file names within the directory
-        - add a dict with the searched strings
-        - call the specific pipeline for the file, sending the file path as a parameter
+    - movs files from a source folder to specific destinations basead on their name as defined
+      in the routing dictionary
+    - case-insensitive
     """
 
-    for file in path_folder.iterdir():
-        if file.is_file() and file.suffix == '.csv':
-            for key, pipeline_func in FILE_ROUTER.items():
-                if key in file.name:
-                    pipeline_func(path=file)
+    if not os.path.isdir(input_folder):
+        logger.critical(f'diretorio informado nao existe: {input_folder}', extra={
+            'job': 'forward_files_by_name',
+            'status': 'failure'
+        })
+        raise ValueError
+    
+    files = os.listdir(input_folder)
+
+    for file in files:
+        file_lower = file.lower()
+        match_found = False
+
+        for route_key, destination in file_router.items():
+            if route_key.lower() in file_lower:
+                match_found = True
+
+                if not destination:
+                    logger.critical(f'sem destino definido para "{route_key}"', extra={
+                        'job': 'forward_files_by_name',
+                        'status': 'continue'
+                    })
                     break
+
+                path_origin = os.path.join(input_folder, file)
+                path_destination = os.path.join(destination, file)
+                
+                os.makedirs(destination, exist_ok=True)
+                shutil.move(path_origin, path_destination)
+
+                logger.info(f'"{file}" movido -> "{destination}"', extra={
+                    'job': 'forward_files_by_name',
+                    'status': 'success'
+                })
+                break
+
+        if not match_found:
+            logger.info(f'"{file}" ignorado por nao conter nenhum padrao de FILE_ROUTER', extra={
+                'job': 'forward_files_by_name',
+                'status': 'skipped'
+            })
