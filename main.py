bdd02beafbbc9fbe01038241d0b4d522b1ce0186
Vinicius-Giovanni <viniciusgiovanni2003@gmail.com>
2025-08-04 09:44:17 -0300

    test: testing the mutifactor instance

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from utils.config_logger import setup_logger, log_with_context
 from web_data_collector.login import login_csi
 from config.settings import TEMP_DIR, FILE_ROUTER, DATA_PATHS
 from web_data_collector.olpn import data_extraction_olpn
-from web_data_collector.picking import data_extraction_picking
+from web_data_collector.cancel import data_extraction_cancel
 from pipelines.standard_pipeline.olpn_pipeline import OlpnPipeline
 
 # remote imports
@@ -24,7 +24,7 @@ def main():
         sys.exit(1)
 
     t1 = threading.Thread(target=data_extraction_olpn, args=(cookies, TEMP_DIR['BRONZE']['olpn']))
-    t2 = threading.Thread(target=data_extraction_picking)
+    t2 = threading.Thread(target=data_extraction_cancel, args=(cookies, TEMP_DIR['BRONZE']['cancel']))
 
     t1.start()
     t2.start()
