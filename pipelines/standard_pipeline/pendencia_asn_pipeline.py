ff3eaae74fa7bbf154eff3793adb60afb1ed73c2
Vinicius-Giovanni <viniciusgiovanni2003@gmail.com>
2025-11-17 17:04:24 -0300

    Pendencias ASN Pipeline

--- pipelines/standard_pipeline/pendencia_asn_pipeline.py
+++ pipelines/standard_pipeline/pendencia_asn_pipeline.py
@@ -4,10 +4,9 @@ import numpy as np
 from config.pipeline_config import PIPELINE_CONFIG
 from utils.config_logger import log_with_context
 from config.pipeline_config import logger
-from utils.classification import classify_setores, check_deadline
 from utils.reader import read_csv, export_as_parquet
 
-@Log_with_context(job='PendenciaASNPipeline', logger=logger)
+@log_with_context(job='PendenciaASNPipeline', logger=logger)
 class PendenciaASNPipeline:
 
     def __init__(self):
@@ -23,4 +22,42 @@ class PendenciaASNPipeline:
 
         output_path.mkdir(parents=True, exist_ok=True)
 
+        df = read_csv(
+            path=input_path,
+            pipeline_key=self.key
+        )
+
+        if df.empty:
+            logger.critical('dataframe vazio', extra={'status':'critico'})
+            return pd.DataFrame()
+        
+        df = self.preprocess(df)
+
+        export_as_parquet(df, output_folder=output_path, pipeline_key=self.key, name='1.05 - Pendencia de fechamento de ASN')
+
+        logger.info(f'pipeline "{self.key}" finalizado com sucesso',extra={'status':'sucesso'})
+
+        return df
+    
+    def preprocess(self, df: pd.DataFrame) -> pd.DataFrame:
+        cfg = self.cfg
+        
+        df.drop(columns=cfg.get('remove_columns', []),errors='ignore', inplace=True)
+        df.rename(columns=cfg.get('rename_columns',{}), inplace=True)
+
+        for col, dtype in cfg.get('column_types',{}).items():
+            if col in df.columns:
+                df[col] = df[col].astype(dtype)
+
+        for col in cfg.get('datetime_columns', []):
+            if col in df.columns:
+                df[col] = pd.to_datetime(df[col], errors='coerce')
+
+        if 'data_integracao_wms' in df.columns:
+            df['data_criterio'] = df['data_integracao_wms'].dt.strftime('%d-%m-%Y')
+            df['hora'] = df['data_integracao_wms'].dt.strftime('%H:00:00')
+        else:
+            logger.critical('coluna "data_integracao_wms" nao encontrada no dataframe', extra={'status':'critico'})
+        
+        return df
         
